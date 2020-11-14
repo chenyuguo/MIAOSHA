9f041901b1cb94f584e05219a0977a00109a73cd
chenyuguo <49013160+chenyuguo@users.noreply.github.com>
2020-11-14 23:16:52 +0800

    second

--- src/main/java/com/miaosha/demo/controller/LoginController.java
+++ src/main/java/com/miaosha/demo/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.miaosha.demo.Controller;
+package com.miaosha.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
